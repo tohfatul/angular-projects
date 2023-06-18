56002d1ca36d997bc24eb7e83d0addda4df852fd
tohfatul <tohfatul.iota@gmail.com>
2023-06-17 21:34:15 -0400

    more updates

--- angularForm/public/angular-app/src/app/app.component.ts
+++ angularForm/public/angular-app/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'models/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit{
+  topics=['Angular', 'React', 'Vue'];
+
+  userModel:User = new User('Rob', 'rob@test.com', 5556665566, 'default', 'morning', true);
+
+  topicHasError=true;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  validateTopic(val:string){
+
+    if(val==='default'){
+      this.topicHasError=true;
+
+    }else{
+      this.topicHasError=false;
+    }
+  }
+
+  onSubmit(){
+    
+  }
 }
---
+++ angularForm/public/angular-app/src/app/enrollment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { User } from 'models/User';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+
+  _url='';
+
+  constructor(private _http:HttpClient) { }
+
+  enroll(user:User){
+    return this._http.post<any>(this._url, user);
+
+  }
+}
